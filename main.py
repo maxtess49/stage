84a5711c69638ee44b73c6c194f2e98eb7e9c559
maxence <marot.maxence@gmail.com>
2022-03-03 11:08:04 +0100

    Instances initialisation (DONE)

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 class Item:
-    profit = 0
-    weight = []
+
+    def __init__(self):
+        self.profit = 0
+        self.weight = []
+
 
     def __str__(self):
-        return "Profit: " + str(self.profit) + "Weight: " + str(self.weight)
-
+        return "Profit: " + str(self.profit) + " Weight: " + str(self.weight)
 
 def open_instance(path):
     """
     Open the MKP instances and create the needed variables (nbr instances, objects, knapsack)
 
     :param path: The path to the file
-    :return: The number of instances in the file, the objects and the knapsack size
+    :return: Each item, the knapsack size and the optimum value of the problem (0 if unknown)
     """
 
     nbr_instances = 1
@@ -24,9 +26,10 @@ def open_instance(path):
 
     with open(path, "r") as file:
 
-        # Remove the first object of the line because it's a blank
-        firstLine = file.readline()[1:]
-        firstLine = firstLine.split(" ")
+        # Remove the first object of the line if it's a blank
+        firstLine = file.readline().split(" ")
+        if firstLine[0] == "":
+            firstLine = firstLine[1:]
 
         # Get nbr of instances if defined
         if firstLine[1] == "\n":
@@ -44,25 +47,35 @@ def open_instance(path):
             # Get profits
             i = 0
             while i < nbr_items:
-                line = file.readline()[1:].split()[:-1]
-                for item_iter in range(len(line)-1):
+                line = file.readline()[1:].split()
+                for profit in line:
                     item = Item()
-                    item.profit = line[item_iter]
+                    item.profit = int(profit)
                     items[instance] += [item]
                 i += len(line)
 
+            i = 0
+            j = 0
             # Get weights
-            for i in range(nbr_constraints):
-                for j in range(nbr_items):
-                    line = file.readline()[1:].split()[:-1]
-                    for item_iter in range(len(line)-1):
-                        item.weight += [line[item_iter]]
-                    i += len(line)
+            while i < nbr_items * nbr_constraints:
+                line = file.readline()[1:].split()
+                for weight in line:
+                    if j % nbr_items == 0:
+                        j = 0
+                    items[instance][j].weight += [weight]
+                    j += 1
+                i += len(line)
 
             # Get knapsack sizes
-            file.readline()
-            firstLine = file.readline()[1:].split(" ")
-            print(firstLine[:-1])
+            line = file.readline()[1:].split()
+            knapsack += [[]]
+            for size in line:
+                knapsack[instance] += [int(size)]
 
+            # For next iteration
+            firstLine = file.readline()[1:].split(" ")[:-1]
 
-open_instance("instances/chubeas/OR5x100/OR5x100.dat")
+    return (items, knapsack, optimum_value)
+
+#open_instance("instances/sac94/hp/hp1.dat")
+#open_instance("instances/chubeas/OR5x100/OR5x100.dat")
